a36946c3ca1f10597722ed666dc2bd745f4c5a5e
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-01-10 00:00:54 -0500

    fixing
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const http = require('http');
 const express = require('express');
 const path = require('path');
 const usersRoute = require('./routes/usersRoute');
 const homeRoute = require('./routes/homeRoute'); 
 
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/users', usersRoute); 
 app.use(homeRoute); 
 
-server.listen(3000);
+app.listen(3000);

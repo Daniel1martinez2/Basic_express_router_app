40d65eedda07559e49e9216301d262ed5221ef73
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2022-01-10 00:50:15 -0500

    body parser
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+//Routes
 const usersRoute = require('./routes/usersRoute');
-const homeRoute = require('./routes/homeRoute'); 
+const homeRoute = require('./routes/homeRoute');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use('/users', usersRoute); 
 app.use(homeRoute); 
 
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router(); 
 
 router.get('/', (req, res)=>{
+  //if there is no body parser -> the console log below will throw undefined
+  console.log(req.body);
   res.sendFile(path.join(__dirname, '..', 'views', 'users.html'))
 })
 
